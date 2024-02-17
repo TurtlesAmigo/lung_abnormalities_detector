d9e35f54855a5db5de5e0d707b836c1e4b0951a0
TurtlesAmigoPC <turtlesamigo@gmail.com>
2024-02-17 02:01:51 +0200

    Add addTreeTableViewFlagListener utils method

--- src/main/java/com/turtlesamigo/utils/UIUtils.java
+++ src/main/java/com/turtlesamigo/utils/UIUtils.java
@@ -1,6 +1,8 @@
 package com.turtlesamigo.utils;
 
+import com.turtlesamigo.controllers.helpers.TreeTableRecordItem;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TreeItem;
 
 public class UIUtils {
     public static void showAlert(String title, String headerText, String message, Alert.AlertType alertType) {
@@ -9,5 +11,20 @@ public class UIUtils {
         alert.setHeaderText(headerText);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    /**
+     * Adds a listener to the TreeTableView item to update the visibility of the child items.
+     * If the item is hidden, all child items will be hidden as well. If the item is shown, all
+     * child items will be shown as well.
+     * @param item The item to add the listener to.
+     * @param treeNode The tree node to update the visibility of the child items.
+     */
+    public static void addTreeTableViewFlagListener(TreeTableRecordItem item, TreeItem<TreeTableRecordItem> treeNode) {
+        item.isShownProperty().addListener((observable, oldValue, newValue) -> {
+            for (var recordNode : treeNode.getChildren()) {
+                recordNode.getValue().setIsShown(newValue);
+            }
+        });
     }
 }
