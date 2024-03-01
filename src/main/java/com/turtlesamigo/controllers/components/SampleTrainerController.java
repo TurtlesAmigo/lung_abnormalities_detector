596c1f119845f3ccc8c0fac4572f5ac7f80787c0
TurtlesAmigoPC <turtlesamigo@gmail.com>
2024-03-02 00:38:37 +0200

    Create UI component for training

---
+++ src/main/java/com/turtlesamigo/controllers/components/SampleTrainerController.java
@@ -0,0 +1,11 @@
+package com.turtlesamigo.controllers.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class SampleTrainerController {
+    @FXML public void onTrainButtonClicked(MouseEvent mouseEvent) {
+        // train ResNet50 model
+
+    }
+}
